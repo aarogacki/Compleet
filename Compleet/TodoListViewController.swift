b2e97051c93c28fbffed1ce26b274bb549eb3f55
aarogacki <alainarogacki@gmail.com>
2018-09-03 12:45:57 -0400

    init commit

---
+++ Compleet/TodoListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Compleet
+//
+//  Created by Alaina Rogacki on 9/3/18.
+//  Copyright © 2018 Alaina Rogacki. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
